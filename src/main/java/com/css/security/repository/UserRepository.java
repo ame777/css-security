5ccf62b322d78e5e7defcb5ad91d27234ae4fe3b
amadou <amandiaye777@gmail.com>
2024-02-15 12:42:05 +0000

    ajout des repositories

---
+++ src/main/java/com/css/security/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.css.security.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.css.security.models.User;
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+
+  Optional<User> findByUsername(String username);
+
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String email);
+}
